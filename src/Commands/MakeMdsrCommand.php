8006577a4d7f9e6d10f1cab930d11e115568537b
Mr Tolouei <alitolouei.1997@gmail.com>
2025-03-02 23:58:13 +0330

    Fix code styles

--- src/Commands/MakeMdsrCommand.php
+++ src/Commands/MakeMdsrCommand.php
@@ -7,22 +7,24 @@ use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\App;
 
-class MakeMdsrCommand extends Command {
+class MakeMdsrCommand extends Command
+{
 
-	/** @var string */
-	public $signature = 'structify:make-mdsr {name}';
+    /** @var string */
+    public $signature = 'structify:make-mdsr {name}';
 
-	/** @var string */
-	public $description = 'Create Model, DTO, Service, and Repository for a given entity.';
+    /** @var string */
+    public $description = 'Create Model, DTO, Service, and Repository for a given entity.';
 
-	/**
-	 * @param Filesystem $file
-	 */
-	public function __construct(
-		protected Filesystem $file,
-	) {
-		parent::__construct();
-	}
+    /**
+     * @param Filesystem $file
+     */
+    public function __construct(
+        protected Filesystem $file,
+    )
+    {
+        parent::__construct();
+    }
 
     /**
      * Execute the console command.
@@ -30,18 +32,19 @@ class MakeMdsrCommand extends Command {
      * @return int
      * @throws FileNotFoundException
      */
-	public function handle(): int {
-		$name = $this->argument('name');
+    public function handle(): int
+    {
+        $name = $this->argument('name');
         $this->createModel($name);
-		$this->createDto($name);
-		$this->createRepositoryInterface($name);
-		$this->createRepository($name);
-		$this->createServiceInterface($name);
-		$this->createService($name);
+        $this->createDto($name);
+        $this->createRepositoryInterface($name);
+        $this->createRepository($name);
+        $this->createServiceInterface($name);
+        $this->createService($name);
         $this->updateMdsrServiceProvider($name);
-		$this->info("MDSR files for $name created successfully!");
+        $this->info("MDSR files for $name created successfully!");
         return 0;
-	}
+    }
 
     /**
      * Get the contents of a stub file.
@@ -50,9 +53,10 @@ class MakeMdsrCommand extends Command {
      * @return string
      * @throws FileNotFoundException
      */
-	protected function getStub(string $type): string {
-		return $this->file->get(__DIR__ . "/../Stubs/mdsr/$type.stub");
-	}
+    protected function getStub(string $type): string
+    {
+        return $this->file->get(__DIR__ . "/../Stubs/mdsr/$type.stub");
+    }
 
     /**
      * Replace placeholders in the stub with the given entity name.
@@ -61,10 +65,10 @@ class MakeMdsrCommand extends Command {
      * @param string $name
      * @return string
      */
-	protected function replacePlaceholders(string $stub, string $name): string
-	{
-		return str_replace('{{Entity}}', $name, $stub);
-	}
+    protected function replacePlaceholders(string $stub, string $name): string
+    {
+        return str_replace('{{Entity}}', $name, $stub);
+    }
 
     /**
      * Ensure the directory exists.
@@ -72,11 +76,12 @@ class MakeMdsrCommand extends Command {
      * @param string $path
      * @return void
      */
-	protected function ensureDirectoryExists(string $path): void {
-		if (!$this->file->isDirectory(dirname($path))) {
-			$this->file->makeDirectory(dirname($path), 0755, true, true);
-		}
-	}
+    protected function ensureDirectoryExists(string $path): void
+    {
+        if (!$this->file->isDirectory(dirname($path))) {
+            $this->file->makeDirectory(dirname($path), 0755, true, true);
+        }
+    }
 
     /**
      * Create a Model file for the given entity.
@@ -84,9 +89,10 @@ class MakeMdsrCommand extends Command {
      * @param string $name
      * @return void
      */
-    protected function createModel(string $name): void {
+    protected function createModel(string $name): void
+    {
         $this->call('make:model', [
-           'name' => $name,
+            'name' => $name,
             '--migration' => true
         ]);
     }
@@ -98,12 +104,13 @@ class MakeMdsrCommand extends Command {
      * @return void
      * @throws FileNotFoundException
      */
-	protected function createDto(string $name): void {
-		$stub = $this->getStub('dto');
-		$path = App::path("Dto/{$name}Dto.php");
-		$this->ensureDirectoryExists($path);
-		$this->file->put($path, $this->replacePlaceholders($stub, $name));
-	}
+    protected function createDto(string $name): void
+    {
+        $stub = $this->getStub('dto');
+        $path = App::path("Dto/{$name}Dto.php");
+        $this->ensureDirectoryExists($path);
+        $this->file->put($path, $this->replacePlaceholders($stub, $name));
+    }
 
     /**
      * Create a Repository Interface file for the given entity.
@@ -112,12 +119,13 @@ class MakeMdsrCommand extends Command {
      * @return void
      * @throws FileNotFoundException
      */
-	protected function createRepositoryInterface(string $name): void {
-		$stub = $this->getStub('repository-interface');
-		$path = App::path("Repositories/Interfaces/{$name}RepositoryInterface.php");
-		$this->ensureDirectoryExists($path);
-		$this->file->put($path, $this->replacePlaceholders($stub, $name));
-	}
+    protected function createRepositoryInterface(string $name): void
+    {
+        $stub = $this->getStub('repository-interface');
+        $path = App::path("Repositories/Interfaces/{$name}RepositoryInterface.php");
+        $this->ensureDirectoryExists($path);
+        $this->file->put($path, $this->replacePlaceholders($stub, $name));
+    }
 
     /**
      * Create a Repository file for the given entity.
@@ -126,12 +134,13 @@ class MakeMdsrCommand extends Command {
      * @return void
      * @throws FileNotFoundException
      */
-	protected function createRepository(string $name): void {
-		$stub = $this->getStub('repository');
-		$path = App::path("Repositories/{$name}Repository.php");
-		$this->ensureDirectoryExists($path);
-		$this->file->put($path, $this->replacePlaceholders($stub, $name));
-	}
+    protected function createRepository(string $name): void
+    {
+        $stub = $this->getStub('repository');
+        $path = App::path("Repositories/{$name}Repository.php");
+        $this->ensureDirectoryExists($path);
+        $this->file->put($path, $this->replacePlaceholders($stub, $name));
+    }
 
     /**
      * Create a Service Interface file for the given entity.
@@ -140,12 +149,13 @@ class MakeMdsrCommand extends Command {
      * @return void
      * @throws FileNotFoundException
      */
-	protected function createServiceInterface(string $name): void {
-		$stub = $this->getStub('service-interface');
-		$path = App::path("Services/Interfaces/{$name}ServiceInterface.php");
-		$this->ensureDirectoryExists($path);
-		$this->file->put($path, $this->replacePlaceholders($stub, $name));
-	}
+    protected function createServiceInterface(string $name): void
+    {
+        $stub = $this->getStub('service-interface');
+        $path = App::path("Services/Interfaces/{$name}ServiceInterface.php");
+        $this->ensureDirectoryExists($path);
+        $this->file->put($path, $this->replacePlaceholders($stub, $name));
+    }
 
     /**
      * Create a Service file for the given entity.
@@ -154,12 +164,13 @@ class MakeMdsrCommand extends Command {
      * @return void
      * @throws FileNotFoundException
      */
-	protected function createService(string $name): void {
-		$stub = $this->getStub('service');
-		$path = App::path("Services/{$name}Service.php");
-		$this->ensureDirectoryExists($path);
-		$this->file->put($path, $this->replacePlaceholders($stub, $name));
-	}
+    protected function createService(string $name): void
+    {
+        $stub = $this->getStub('service');
+        $path = App::path("Services/{$name}Service.php");
+        $this->ensureDirectoryExists($path);
+        $this->file->put($path, $this->replacePlaceholders($stub, $name));
+    }
 
     /**
      * Update or create the MdsrServiceProvider and bind the repository and service interfaces to their implementations.
@@ -183,7 +194,7 @@ class MakeMdsrCommand extends Command {
         $providerContent = $this->file->get($providerPath);
         $bindingCode = '';
         foreach ($bindings as $interface => $implementation) {
-            $bindingCode .= "\n        "."\$this->app->bind($interface::class, $implementation::class);";
+            $bindingCode .= "\n        " . "\$this->app->bind($interface::class, $implementation::class);";
         }
         if (str_contains($providerContent, 'public function register()')) {
             $registerMethodStart = strpos($providerContent, 'public function register()');
--- src/StructifyServiceProvider.php
+++ src/StructifyServiceProvider.php
@@ -5,20 +5,23 @@ namespace Mrtolouei\Structify;
 use Illuminate\Support\ServiceProvider;
 use Mrtolouei\Structify\Commands\MakeMdsrCommand;
 
-class StructifyServiceProvider extends ServiceProvider {
+class StructifyServiceProvider extends ServiceProvider
+{
 
-	public function boot(): void {
-		if ($this->app->runningInConsole()) {
-			$this->commands([
-				MakeMdsrCommand::class,
-			]);
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MakeMdsrCommand::class,
+            ]);
             $this->publishes([
                 __DIR__ . '/Stubs' => base_path('stubs/'),
             ], 'structify-stubs');
-		}
-	}
+        }
+    }
 
-	public function register(): void {
-		//
-	}
+    public function register(): void
+    {
+        //
+    }
 }
