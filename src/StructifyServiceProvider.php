d766613af9cb0fa25075c4c6e5a0e6d2f678457b
Alireza Tolouei <alitolouei.1997@gmail.com>
2025-03-02 17:08:09 +0330

    First commit

---
+++ src/StructifyServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mrtolouei\Structify;
+
+use Illuminate\Support\ServiceProvider;
+
+class StructifyServiceProvider extends ServiceProvider {
+
+	public function boot(): void {
+		//
+	}
+
+	public function register(): void {
+		//
+	}
+}
